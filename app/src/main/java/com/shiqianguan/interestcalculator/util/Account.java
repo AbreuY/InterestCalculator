d3bb540b2cfdf570760350f803bee85758f0a659
guans <guanshiqian@163.com>
2017-06-28 20:07:11 +0800

    add logic

--- app/src/main/java/com/shiqianguan/interestcalculator/util/Account.java
+++ app/src/main/java/com/shiqianguan/interestcalculator/util/Account.java
@@ -1,71 +1,81 @@
-﻿package com.shiqianguan.interestcalculator.util;
+package com.shiqianguan.interestcalculator.util;
 
 import java.math.BigDecimal;
-/*
- * 自定义的一个账户类，属性包括用户名，利率和账户余额
+
+/**
+ * Created by guans on 2017/6/28.
  */
 
 public class Account {
-	private String userName;
-	private BigDecimal annualInterest;
-	private BigDecimal balance;
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	//默认的构造函数，初始化账户金额和利率；
-	public Account(){
-		userName="Default";
-		setBalance(0.00);
-		setBalance(0.00);
-	}
-	public BigDecimal getAnnualInterest() {
-		return annualInterest;
-	}
-	public void setAnnualInterest(double annualInterest) {
-		this.annualInterest = new BigDecimal(annualInterest);
-	}
-	public BigDecimal getBalance() {
-		return balance;
-	}
-	public void setBalance(double balance) {
-		this.balance = new BigDecimal(balance);
-	}
-	/*
-	 * 利息计算，使用的是复利的计算方式；
-	 * 由于题意要求是整年存取，故只考虑存取年限为整年的判断，不再对没存够整年的情况进行分析（否则的话要对年份和利息规则进行计算）
-	 * 考虑到精度使用BigDecimal进行计算;
-	 */
-	public BigDecimal getTotoalBalance(int years){
-		//首先进行输入年限的非负判断
-		if(years <0)
-			throw new IllegalArgumentException("Years cannot be negative");
-	    BigDecimal growthRate=annualInterest.add(new BigDecimal(1));
-		for(int i=0;i<=years-1;i++){
-			balance=balance.multiply(growthRate);
-		}
-		return balance;		
-	}
-	/*
-	 *实现利率计算的静态方法；
-	 @param inputBalance 输入的账户初始金额
-	 @param  inputAnnualInterest 要输入的年利率
-	 @param years 输入的存入年限
-	 @return 计算出来的本金+利息总金额，是一个BigDcimal值，结果可以使用BigDecimal的方法转化为常规数值
-	 */
-	public static BigDecimal getTotoalBalance(double inputBalance,double inputAnnualInterest,int years){
-		//对输入的年限进行检查，如果为负数则直接抛出异常，再进行处理
-		if(years<0)
-			throw new IllegalArgumentException("Years cannot be negative");
-		BigDecimal balance=new BigDecimal(inputBalance);
-		BigDecimal annualInterest=new BigDecimal(inputAnnualInterest);
-		BigDecimal growthRate=annualInterest.add(new BigDecimal(1));
-		for(int i=0;i<=years-1;i++){
-			balance=balance.multiply(growthRate);
-		}
-		return balance;		
-	}
+    private String userName;
+    private BigDecimal annualInterest;
+    private BigDecimal balance;
+
+    //默认的构造函数，初始化账户金额和利率；
+    public Account() {
+        userName = "Default";
+        setBalance(0.00);
+        setBalance(0.00);
+    }
+
+    /*
+     *实现利率计算的静态方法；
+     @param inputBalance 输入的账户初始金额
+     @param  inputAnnualInterest 要输入的年利率
+     @param years 输入的存入年限
+     @return 计算出来的本金+利息总金额，是一个BigDcimal值，结果可以使用BigDecimal的方法转化为常规数值
+     */
+    public static BigDecimal getTotalBalance(double inputBalance, double inputAnnualInterest, int years) {
+        //对输入的年限进行检查，如果为负数则直接抛出异常，再进行处理
+        if (years < 0)
+            throw new IllegalArgumentException("Years cannot be negative");
+        BigDecimal balance = new BigDecimal(inputBalance);
+        BigDecimal annualInterest = new BigDecimal(inputAnnualInterest);
+        BigDecimal growthRate = annualInterest.add(new BigDecimal(1));
+        for (int i = 0; i <= years - 1; i++) {
+            balance = balance.multiply(growthRate);
+        }
+        return balance;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public BigDecimal getAnnualInterest() {
+        return annualInterest;
+    }
+
+    public void setAnnualInterest(double annualInterest) {
+        this.annualInterest = new BigDecimal(annualInterest);
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = new BigDecimal(balance);
+    }
+
+    /*
+     * 利息计算，使用的是复利的计算方式；
+     * 由于题意要求是整年存取，故只考虑存取年限为整年的判断，不再对没存够整年的情况进行分析（否则的话要对年份和利息规则进行计算）
+     * 考虑到精度使用BigDecimal进行计算;
+     */
+    public BigDecimal getTotalBalance(int years) {
+        //首先进行输入年限的非负判断
+        if (years < 0)
+            throw new IllegalArgumentException("Years cannot be negative");
+        BigDecimal growthRate = annualInterest.add(new BigDecimal(1));
+        for (int i = 0; i <= years - 1; i++) {
+            balance = balance.multiply(growthRate);
+        }
+        return balance;
+    }
 
 }
--- app/src/main/java/com/shiqianguan/interestcalculator/view/CalculatorActivity.java
+++ app/src/main/java/com/shiqianguan/interestcalculator/view/CalculatorActivity.java
@@ -1,15 +1,113 @@
 package com.shiqianguan.interestcalculator.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.shiqianguan.interestcalculator.R;
+import com.shiqianguan.interestcalculator.util.Account;
+
+import java.math.BigDecimal;
 
 public class CalculatorActivity extends AppCompatActivity {
+    private EditText balance_edit;
+    private EditText annual_interest_edit;
+    private EditText years_edit;
+    private Button calculate_button;
+    private TextView result_text;
+    private Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+        balance_edit = (EditText) findViewById(R.id.balance);
+        annual_interest_edit = (EditText) findViewById(R.id.annual_interest);
+        years_edit = (EditText) findViewById(R.id.years);
+        calculate_button = (Button) findViewById(R.id.calculate_button);
+        result_text = (TextView) findViewById(R.id.result_text);
+    }
+
+    private void initListener() {
+        EditTextWatcher editTextWatcher = new EditTextWatcher();
+        balance_edit.addTextChangedListener(editTextWatcher);
+        annual_interest_edit.addTextChangedListener(editTextWatcher);
+        years_edit.addTextChangedListener(editTextWatcher);
+        calculate_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkInput()) {
+                    double balance = Double.parseDouble(balance_edit.getText().toString());
+                    double annual_interest = Double.parseDouble(annual_interest_edit.getText().toString()) / 100;
+                    int years = Integer.parseInt(years_edit.getText().toString());
+                    try {
+                        result_text.requestFocus();
+                        //计算时间很短，此次不做异步处理
+                        result_text.setText("计算结果为：" + String.valueOf(Account.getTotalBalance(balance, annual_interest, years).setScale(4, BigDecimal.ROUND_HALF_EVEN)) + "元");
+                        result_text.setVisibility(View.VISIBLE);
+
+                    } catch (IllegalArgumentException e) {
+                        years_edit.requestFocus();
+                        showToast("年份输入不能为负数，请重新输入");
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean checkInput() {
+        if (balance_edit.getText().length() == 0) {
+            balance_edit.requestFocus();
+            showToast("存入金额不能为空");
+            return false;
+        }
+        if (annual_interest_edit.getText().length() == 0) {
+            annual_interest_edit.requestFocus();
+            showToast("年利率不能为空");
+            return false;
+        }
+        if (years_edit.getText().length() == 0) {
+            years_edit.requestFocus();
+            showToast("存入年份不能为空");
+            return false;
+        }
+        return true;
+    }
+
+    private void showToast(String content) {
+        if (mToast == null) {
+            mToast = Toast.makeText(CalculatorActivity.this, content, Toast.LENGTH_SHORT);
+        } else {
+            mToast.setText(content);
+        }
+        mToast.show();
+    }
+
+    private class EditTextWatcher implements TextWatcher {
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            result_text.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
     }
 }
