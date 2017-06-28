bd6dd9bae0d20f91a64548ef6b44a6cacb5228ac
Guans <guanshiqian@163.com>
2017-06-28 13:35:32 +0800

    Initial commit

---
+++ app/src/main/java/com/shiqianguan/interestcalculator/view/CalculatorActivity.java
@@ -0,0 +1,15 @@
+package com.shiqianguan.interestcalculator.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shiqianguan.interestcalculator.R;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
